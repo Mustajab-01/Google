e8c97a0e7e1a6ab84f6c4282bf0c518e16416652
Mustajab-01 <mustajab5014.477@gmail.com>
2023-08-07 12:48:24 +0500

    second commit - add header

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import { TbGridDots } from 'react-icons/tb';
+
+const Header = () => {
+    return (
+        <header className='text-gray-500 w-full gap-4 flex justify-end items-center p-6'>
+            <a href='https://mail.google.com' className='text-xs hover:underline underline-offset-4'>Gmail</a>
+            <a href='https://www.google.com/imghp' className='text-xs hover:underline underline-offset-4'>Images</a>
+            <button className='rounded-full hover:bg-gray-200 transition-all ease-in-out duration-500 p-2 text-xl'>
+                <TbGridDots />
+            </button>
+            <button className='bg-indigo-500 hover:bg-indigo-700 text-white px-6 py-2 rounded-lg transition-all ease-in-out duration-500 font-bold text-sm'>Sign in</button>
+        </header>
+    );
+};
+
+export default Header;
